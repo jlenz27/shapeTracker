209468f7af18db4fe7543d95d5757f27845f1c04
John Lenz <john.lenz27@gmail.com>
2023-02-20 10:29:48 -0800

    it will pass test 3

--- ShapeTracker.Tests/ModelTests/TriangleTests.cs
+++ ShapeTracker.Tests/ModelTests/TriangleTests.cs
@@ -23,12 +23,9 @@ namespace ShapeTracker.Tests
         [TestMethod]
     public void SetSide1_SetsValueOfSide1_Void()
         {
-            // Arrange
             Triangle newTriangle = new Triangle(3);
             int newLength1 = 44;
-            // Act
             newTriangle.Side1 = newLength1;
-            // Assert
             Assert.AreEqual(newLength1, newTriangle.Side1);
         }
     }
