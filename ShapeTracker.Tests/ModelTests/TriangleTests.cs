c0257d0b5add9e4e20e5d05f7df4bc9960bead56
John Lenz <john.lenz27@gmail.com>
2023-02-20 11:40:07 -0800

    pass test 12

--- ShapeTracker.Tests/ModelTests/TriangleTests.cs
+++ ShapeTracker.Tests/ModelTests/TriangleTests.cs
@@ -1,11 +1,18 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using ShapeTracker.Models;
+using System.Collections.Generic; 
+using System;
 
 namespace ShapeTracker.Tests
 {
     [TestClass]
-    public class TriangleTests
+    public class TriangleTests : IDisposable
     {
+        public void Dispose()
+        {
+            Triangle.ClearAll();
+        }
+        
         [TestMethod]
         public void TriangleConstructor_CreatesInstanceOfTriangle_Triangle()
         {
