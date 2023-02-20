888761d125a2198c90d671b539942f6c6f12ecfa
John Lenz <john.lenz27@gmail.com>
2023-02-20 11:22:30 -0800

    good fail test 9

--- ShapeTracker.Tests/ModelTests/TriangleTests.cs
+++ ShapeTracker.Tests/ModelTests/TriangleTests.cs
@@ -74,5 +74,15 @@ namespace ShapeTracker.Tests
             string triType = notATriangle.CheckType();
             Assert.AreEqual("not a triangle", triType);
         }
+        [TestMethod]
+        public void CheckType_DeterminesWhenScalene_String()
+        {
+            // Arrange
+            Triangle scaleneTri = new Triangle(3, 4, 5);
+            // Act
+            string triType = scaleneTri.CheckType();
+            // Assert
+            Assert.AreEqual("scalene triangle", triType);
+        }
     }
 }
